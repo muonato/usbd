e5448faeda602047a3913f8fa492553e8714ffcc
Tommi Muona <43796235+muonato@users.noreply.github.com>
2023-02-19 08:04:26 +0200

    Create usbd.ps1

---
+++ usbd.ps1
@@ -0,0 +1,15 @@
+# Detect USB drive insertion / removal
+#
+# Powershell snippet to notify the user with popup message (msg.exe) on USB drive
+# insertion and removal. Message should be included in a file called 'notice.txt'
+# 
+# Examples:
+# 		'powershell.exe -windowstyle hidden -file detect-usb.ps1' to hide
+# 		'Unregister-Event -SourceIdentifier USBDrive' to halt event mgmt after use
+#
+# Article:
+#     https://stackoverflow.com/questions/21805166/start-powershell-script-when-usb-drive-is-inserted
+
+$Action = { Invoke-Expression "& type .\notice.txt|msg.exe *"; };
+Register-WmiEvent -Class win32_VolumeChangeEvent -Action $Action -SourceIdentifier USBDrive;
+$newEvent = Wait-Event -SourceIdentifier USBDrive;
